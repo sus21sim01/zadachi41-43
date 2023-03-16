73681fa3b986b355d22293539308177f2607a5fc
susanna <sus@gmail.com>
2023-03-16 19:10:40 +0300

    zadacha41

---
+++ zadchiles6/zdacha41.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+int[] InputArray (int M) 
+{
+ int[] array = new int[M];
+ for (int i = 0; i < M; i++)
+    {
+ Console.WriteLine($"Введите {i + 1} число: ");
+ array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+ for (int i = 0; i < M; i++)
+    {
+ if (i == M - 1) Console.Write($"{array[i]} -> ");
+ else Console.Write($"{array[i]}, "); 
+    }
+ return array;
+}
+
+int PositiveNumbersCounter(int[] array) 
+{
+ int counter = 0;
+ for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) 
+        {
+            counter++;
+        }
+    }
+    Console.Write(counter);
+    return counter;
+}
+
+PositiveNumbersCounter(InputArray(5));
